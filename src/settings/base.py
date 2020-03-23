62f429f2ea8d9f207ff64e84b9c0f7d113cfb018
Ivan Podkidyshev <raashicat@gmail.com>
2020-03-23 23:57:38 +0400

    cors

--- src/settings/base.py
+++ src/settings/base.py
@@ -39,11 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'hypertest.user',
     'hypertest.main',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
