9425d37ede6b207234a4c988db2adff7d741d383
Ivan Podkidyshev <raashicat@gmail.com>
2020-03-25 23:21:49 +0400

    one more django-admin enhancement

--- src/hypertest/main/admin.py
+++ src/hypertest/main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Test, Result, Question
+from .models import Test, Result, Question, Answer
 
 
 class TestResultAdminInline(admin.TabularInline):
@@ -15,4 +15,14 @@ class TestAdmin(admin.ModelAdmin):
     inlines = [TestResultAdminInline, TestQuestionAdminInline]
 
 
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+
 admin.site.register(Test, TestAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Result)
