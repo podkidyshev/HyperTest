ee3c4efc83c336e2128e76ffd9e091cfaa841117
Ivan Podkidyshev <raashicat@gmail.com>
2020-04-04 16:16:42 +0400

    all tests work

--- src/tests/api/client/test.py
+++ src/tests/api/client/test.py
@@ -48,7 +48,7 @@ class HyperTestTestCase(AuthenticatedTestCase):
         'title': 'title',
         'description': 'description',
         'picture': None,
-        'isPublished': False,
+        'isPublished': True,
         'vip': False,
         'price': 1,
         'gender': 0,
@@ -117,48 +117,97 @@ class HyperTestTestCase(AuthenticatedTestCase):
         if answers_count is not None:
             self.assertEqual(Answer.objects.count(), answers_count)
 
-    def test_create_and_list(self):
-        response_1 = self.client.post(self.url, self.template, format='json')
-        self.assertEqual(response_1.status_code, 201)
-        response_1_data = response_1.json()
-        self.assertEqual(dict(self.template, id=response_1_data['id']), response_1_data)
+    def make_expected_data(self, response_data, template=None):
+        template = self.template if template is None else template
+        return dict(
+            template,
+            id=response_data['id'],
+            isPublished=False,
+            passed=False,
+            passedCount=0,
+            user=self.user.id
+        )
 
+    def test_create(self):
+        # creation is not allowed with /api/tests
+        self.assertEqual(self.client.post(self.url, self.template, format='json').status_code, 405)
+
+        # you should create tests via /api/tests/my
+        response = self.client.post(self.url_my, self.template, format='json')
+        self.assertEqual(response.status_code, 201)
+
+        # check response
+        response_data = response.json()
+        self.assertEqual(response_data, self.make_expected_data(response_data))
+
+        # test that there is correct number of related objects
         self.assert_objects_count(1, 2, 2, 4)
 
-        response_2 = self.client.post(self.url, self.template, format='json')
-        self.assertEqual(response_2.status_code, 201)
-
+        # create another one and check number of related
+        self.client.post(self.url_my, self.template, format='json')
         self.assert_objects_count(2, 4, 4, 8)
 
-        response_list = self.client.get(self.url)
-        self.assertEqual(response_list.status_code, 200)
+    def test_lists(self):
+        test_data_1 = self.client.post(self.url_my, self.template, format='json').json()
+        test_data_2 = self.client.post(self.url_my, self.template, format='json').json()
 
-        data_list = response_list.json()['items']
-        self.assertEqual(len(data_list), 2)
-        self.assertEqual(data_list[0]['id'], response_2.json()['id'])
-        self.assertEqual(data_list[1]['id'], response_1.json()['id'])
+        # created tests are not published so they don't appear in /api/tests
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['items'], [])
+
+        # they appear in /api/tests/my
+        response = self.client.get(self.url_my)
+        self.assertEqual(response.status_code, 200)
+        items = response.json()['items']
+        self.assertEqual(items[0]['id'], test_data_2['id'])
+        self.assertEqual(items[1]['id'], test_data_1['id'])
+
+    def test_publishing_test(self):
+        test = Test.objects.create(published=True, user=self.user)
+
+        url_test = reverse('tests-detail', [test.id])
+        url_test_my = reverse('tests-my-detail', [test.id])
+
+        # published test is accessible with both /api/tests and /api/tests/my
+        self.assertEqual(len(self.client.get(self.url).json()['items']), 1)
+        self.assertEqual(len(self.client.get(self.url_my).json()['items']), 1)
+        # and detail
+        self.assertEqual(self.client.get(url_test).status_code, 200)
+        self.assertEqual(self.client.get(url_test_my).status_code, 200)
+
+        # now unpublish test
+        test.published = False
+        test.save()
+
+        # when test in not published it is accessible only with /api/tests/my
+        self.assertEqual(len(self.client.get(self.url).json()['items']), 0)
+        self.assertEqual(len(self.client.get(self.url_my).json()['items']), 1)
+        # and detail
+        self.assertEqual(self.client.get(url_test).status_code, 404)
+        self.assertEqual(self.client.get(url_test_my).status_code, 200)
 
     def test_delete(self):
-        data_1 = self.client.post(self.url, self.template, format='json').json()
-        data_2 = self.client.post(self.url, self.template, format='json').json()
+        data_1 = self.client.post(self.url_my, self.template, format='json').json()
+        data_2 = self.client.post(self.url_my, self.template, format='json').json()
 
-        self.client.delete(reverse('tests-detail', [data_1['id']]))
+        self.client.delete(reverse('tests-my-detail', [data_1['id']]))
 
         self.assert_objects_count(1, 2, 2, 4)
 
-        self.client.delete(reverse('tests-detail', [data_2['id']]))
+        self.client.delete(reverse('tests-my-detail', [data_2['id']]))
 
         self.assert_objects_count(0, 0, 0, 0)
 
     def test_update(self):
         data = deepcopy(self.template)
 
-        test_id = self.client.post(self.url, data, format='json').json()['id']
-        url = reverse('tests-detail', [test_id])
+        test_id = self.client.post(self.url_my, data, format='json').json()['id']
+        url = reverse('tests-my-detail', [test_id])
         data['id'] = test_id
 
         # and create another one to be sure there is no conflicts
-        self.client.post(self.url, data, format='json')
+        self.client.post(self.url_my, data, format='json')
 
         # test delete one answer
         data['questions'][1]['vars'].pop()
@@ -166,7 +215,10 @@ class HyperTestTestCase(AuthenticatedTestCase):
         self.assertEqual(response.status_code, 200)
 
         self.assert_objects_count(2, 4, 4, 7)
-        self.assertEqual(data, response.json())
+        response_data = response.json()
+        expected_data = self.make_expected_data(response_data, template=response_data)
+        expected_data['isPublished'] = True
+        self.assertEqual(expected_data, response_data)
 
         # test incorrect result_id in answer (and put method)
         data['questions'][1]['vars'][0]['res'] = 100500
@@ -175,11 +227,13 @@ class HyperTestTestCase(AuthenticatedTestCase):
         self.assertEqual(
             response.json(),
             {
-                'fields': {
-                    'questions': [
-                        {},
-                        {'vars': [{'res': 'res = 100500 does not exist'}]}
-                    ]
+                'errors': {
+                    'fields': {
+                        'questions': [
+                            {},
+                            {'vars': [{'res': 'res = 100500 does not exist'}]}
+                        ]
+                    }
                 }
             }
         )
@@ -193,29 +247,33 @@ class HyperTestTestCase(AuthenticatedTestCase):
         self.assertEqual(
             response.json(),
             {
-                'fields': {
-                    'results': [
-                        {}, {}, {'resId': 'Это поле обязательно'}
-                    ],
-                    'questions': [
-                        {'vars': [{}, {}, {'varId': 'Это поле обязательно'}]},
-                        {'vars': [{'res': 'res = 100500 does not exist'}]},
-                        {'qId': 'Это поле обязательно'}
-                    ]
+                'errors': {
+                    'fields': {
+                        'results': [
+                            {}, {}, {'resId': 'Это поле обязательно'}
+                        ],
+                        'questions': [
+                            {'vars': [{}, {}, {'varId': 'Это поле обязательно'}]},
+                            {'vars': [{'res': 'res = 100500 does not exist'}]},
+                            {'qId': 'Это поле обязательно'}
+                        ]
+                    }
                 }
             }
         )
 
         # test required fields on top level
-        response = self.client.post(self.url, {}, format='json')
+        response = self.client.post(self.url_my, {}, format='json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(
             response.json(),
             {
-                'fields': {
-                    'results': 'Обязательное поле.',
-                    'title': 'Обязательное поле.',
-                    'questions': 'Обязательное поле.'
+                'errors': {
+                    'fields': {
+                        'results': 'Обязательное поле.',
+                        'title': 'Обязательное поле.',
+                        'questions': 'Обязательное поле.'
+                    }
                 }
             }
         )
@@ -233,11 +291,11 @@ class HyperTestTestCase(AuthenticatedTestCase):
             'picture': self.pic
         }
 
-        response = self.client.post(self.url, data, format='json')
+        response = self.client.post(self.url_my, data, format='json')
         self.assertEqual(response.status_code, 201)
 
         data = response.json()
-        url = reverse('tests-detail', [data['id']])
+        url = reverse('tests-my-detail', [data['id']])
 
         # test ignoring url
         response = self.client.put(url, data, format='json')
