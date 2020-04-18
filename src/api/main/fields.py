c450d45503abc86b65ee46e065a53a38ae0d3982
Ivan Podkidyshev <raashicat@gmail.com>
2020-04-18 12:55:42 +0400

    fixed picture url serializing

--- src/api/main/fields.py
+++ src/api/main/fields.py
@@ -55,6 +55,6 @@ class PictureField(ImageField):
 
         request = self.context.get('request', None)
         if request is not None:
-            return request.scheme + '://' + request.get_host() + '/' + value.url
+            return request.META['HTTP_REFERER'] + '/' + value.url
 
         return url
